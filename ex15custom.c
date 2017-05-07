f71aa8f2ce92420641eb32a947e527b9a9ffaa70
Michael Bruce <mike@bearmetalcoding.com>
2017-05-08 00:02:49 +0100

    Example 15 & started string replacement 15custom

---
+++ ex15custom.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// My own extension to structs and pointers
+
+char* substitute(char* string, char* pattern, char* replacement)
+{
+    return "yes";
+}
+
+int main(int argc, char** argv)
+{
+    printf("%s\n", substitute("one", "two", "three"));
+}
